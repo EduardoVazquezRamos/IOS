3e2205d4224aab65087f361ecaf59ae93fdff3c7
ADMINISTRADORUTM <ADMINUTM@imac-de-tecmilenio.local>
2016-12-06 09:46:22 -0600

    First entry

--- AppDemo/ViewController.swift
+++ AppDemo/ViewController.swift
@@ -8,18 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource, UITableViewDelegate{
+    
+let arreglo = [("Eduardo","1"), ("Luisa", "2"), ("Alan", "3"), ("Dulio","4")]
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        print("Vista cargada")
+        
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+ 
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        
+        return arreglo.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let proto = (indexPath.row % 2 == 0) ? "proto1" : "proto2"
+        let vista = tableView.dequeueReusableCell(withIdentifier: proto, for: indexPath) as! FilaTableViewCell
+        vista.lblIzquierda.text = arreglo[indexPath.row].0
+        vista.lblDerecha.text = arreglo[indexPath.row].1
+        
+        return vista
+    }
+    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        print("Fila\(indexPath.row)")
+    }
 
 }
 
